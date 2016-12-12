b0e5183a0bed356c9c20e70c82ebdd0d80bf5e0e
Rosie Highsmith <ariel.highsmith@gmail.com>
2016-12-12 00:16:03 +0000

    Adjust resolution for mentor images

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -6,7 +6,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  storage :aws
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  process resize_to_fill: [800,350]
+  process resize_to_fill: [600,350]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
