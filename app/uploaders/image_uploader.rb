fa3ca994a441cf28cd06e1d18291b9dcb249d090
Rosie Highsmith <ariel.highsmith@gmail.com>
2016-12-12 01:48:57 +0000

    mentor card sizing

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   # version :thumb do
-  process resize_to_fill: [600,350]
+  process resize_to_fill: [600,400]
   # end
 
   # Add a white list of extensions which are allowed to be uploaded.
