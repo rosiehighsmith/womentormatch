08ce499ed353dbf0d32128bc5548c0693356aa87
Rosie Highsmith <ariel.highsmith@gmail.com>
2017-01-03 13:12:43 -0800

    Add sunspot gem for search

--- app/models/mprofile.rb
+++ app/models/mprofile.rb
@@ -1,9 +1,11 @@
 class Mprofile < ActiveRecord::Base
-  mount_uploader :image, ImageUploader
-  belongs_to :user
-  has_many :sections
-  has_many :enrollments
-  validates :first_name, presence: true
-  validates :bio, presence: true
-  validates :email, presence: true
+  searchable do
+    mount_uploader :image, ImageUploader
+    belongs_to :user
+    has_many :sections
+    has_many :enrollments
+    validates :first_name, presence: true
+    validates :bio, presence: true
+    validates :email, presence: true
+  end
 end
